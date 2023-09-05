de97a12e708db2000e0268bd20a73aa042976c59
Coshiloco <pablohurtadohg@gmail.com>
2023-09-05 17:19:47 +0200

    Add CurrentAccount and SavingsAccount classes

---
+++ PHBank/src/components/CurrentAccount.java
@@ -0,0 +1,7 @@
+package components;
+
+public class CurrentAccount extends Account {
+    public CurrentAccount(String label, Client client) {
+        super(label, client);
+    }
+}
---
+++ PHBank/src/components/SavingsAccount.java
@@ -0,0 +1,7 @@
+package components;
+
+public class SavingsAccount extends Account {
+    public SavingsAccount(String label, Client client) {
+        super(label, client);
+    }
+}
